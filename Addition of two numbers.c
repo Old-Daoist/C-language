50586513769184e86df5e82edb7b864ec708c8e9
Khumba Lunganlung <187429161+Old-Daoist@users.noreply.github.com>
2025-07-17 14:30:43 +0530

    Add files via upload

---
+++ Addition of two numbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+ int a,b;
+ printf("Enter two integers to add\n");
+ scanf("%d%d",&a,&b);
+ printf("The output of addition of two numbers is=%d\n",a+b);
+ return 0;
+}
---
+++ Multiuplication of two numbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+ int a,b;
+ printf("Enter two integers to multiply\n");
+ scanf("%d%d",&a,&b);
+ printf("The output of multiplication of two numbers is=%d\n",a*b);
+ return 0;
+}
